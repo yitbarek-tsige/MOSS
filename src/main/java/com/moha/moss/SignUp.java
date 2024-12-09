54a6affd802d06572db9f69f492e93b10d0e900c
Yitbarek Tsige <yitbarektsige@outlook.com>
2024-12-09 11:50:03 +0300

    Update SignUp.java

--- src/main/java/com/moha/moss/SignUp.java
+++ src/main/java/com/moha/moss/SignUp.java
@@ -1,10 +1,8 @@
 package com.moha.moss;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
